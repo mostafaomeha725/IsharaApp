5ccc5c5e34293581321058f45a0a0ce26eb4d58a
unknown <mostafa202202537@gmail.com>
2025-10-11 14:56:33 +0200

    add dark and light mode

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,35 @@
+import 'package:chucker_flutter/chucker_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:isharaapp/main.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import '/core/env.dart';
+import 'route_observer.dart';
+import 'route_paths.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+final CustomGoRouterObserver customGoRouterObserver = CustomGoRouterObserver();
+
+GoRouter createRouter({
+  required VoidCallback onToggleTheme,
+  required ThemeMode themeMode,
+}) {
+  return GoRouter(
+    initialLocation: Routes.splashScreen,
+    navigatorKey: navigatorKey,
+    debugLogDiagnostics: true,
+    observers: [
+      if (isDevEnviroment()) ChuckerFlutter.navigatorObserver,
+      SentryNavigatorObserver(),
+      // customGoRouterObserver,
+    ],
+    routes: [
+      GoRoute(
+        path: Routes.splashScreen,
+        builder: (context, state) =>
+            SplashScreen(onToggleTheme: onToggleTheme, themeMode: themeMode),
+      ),
+    ],
+  );
+}
